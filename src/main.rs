c8991444967c72011925de8db2c050750654eeef
AlecRabbit <alec.therabbit.git@gmail.com>
2021-11-30 23:48:16 +0200

    ~wp

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 #![deny(warnings)]
-
 use warp::Filter;
 use uuid::Uuid;
 use ulid::Ulid;
-// use uuid::v1::{Timestamp, Context};
-// use std::time::{SystemTime};
 use serde_derive::{Serialize};
 
 #[derive(Serialize)]
@@ -24,17 +21,6 @@ struct UuidV4UID {
 
 #[tokio::main]
 async fn main() {
-    pretty_env_logger::init();
-
-    // let promote = warp::get()
-    //     .and(warp::path!("uid" / "ulid" ))
-    //     .map(|| {
-    //         let u = UID { data:  Ulid::new().to_string() };
-    //         warp::reply::json(&u)
-    //     });
-    //
-    // warp::serve(promote).run(([127, 0, 0, 1], 4040)).await
-
 
     let ulid =
         warp::path!("ulid")
@@ -59,6 +45,7 @@ async fn main() {
                     };
                 warp::reply::json(&r)
             });
+
     let uid = warp::path("uid").and(
         ulid
             .or(uuid_v4)
